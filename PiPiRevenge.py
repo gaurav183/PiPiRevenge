2a82d7c50d2c3995ea46cfa8ecc9a62225e61589
Namrita Murali <namrita.murali@gmail.com>
2016-03-11 07:36:24 -0500

    Added asteroids falling

--- PiPiRevenge.py
+++ PiPiRevenge.py
@@ -9,18 +9,19 @@ class PiPiRevenge(EventBasedAnimationClass):
         super(PiPiRevenge, self).__init__(600,800)
         
     def initAnimation(self):
-        self.isMainScreen = True
+        self.isMainScreen = True 
         self.isSplashScreen = False 
         self.isHelpScreen = False
         self.timerDelay = 75
         self.startButtonX0,self.startButtonY0 = self.width/2-80,self.height/2.5
         self.startButtonX1 = self.width/2+80
         self.startButtonY1 = self.startButtonY0 + 70
+        self.startHeight = self.height/4
 
     def drawSplashScreen(self):
         self.background = PhotoImage(file = "images/splashScreen.gif")
         self.canvas.create_image(self.width/2, self.height/2, image = self.background)
-        self.canvas.create_text(self.width/2, self.height/4, text="Pi Pi Revenge", font="BritannicBold 76", fill="white")
+        self.canvas.create_text(self.width/2, self.height/4, text="Pi Pi Revenge", font="Lato 76", fill="white")
         self.drawSplashScreenButtons()
 
     def drawSplashScreenButtons(self):
@@ -36,6 +37,19 @@ class PiPiRevenge(EventBasedAnimationClass):
         self.canvas.create_image((self.width/4), (7*self.height/8), image = self.redPlanet)
         self.canvas.create_image((2*self.width/4), (7*self.height/8), image = self.goldPlanet)
         self.canvas.create_image((3*self.width/4), (7*self.height/8), image = self.bluePlanet)
+        self.drawAsteroids()
+    
+    def onTimerFired(self):
+        self.startHeight += 15
+        if (self.startHeight >= (7*self.height/8)):
+          self.startHeight = self.height/4
+
+    def drawAsteroids(self):
+        self.asteroid = PhotoImage(file = "images/asteroid.gif")
+        self.canvas.create_image((self.width/4), self.startHeight, image = self.asteroid)
+        self.canvas.create_image((self.width/2), self.startHeight, image = self.asteroid)
+        self.canvas.create_image((3*self.width/4), self.startHeight, image = self.asteroid)
+
     def drawStartButton(self):
         x0,y0 = self.startButtonX0,self.startButtonY0
         x1,y1 = self.startButtonX1,self.startButtonY1
@@ -43,7 +57,7 @@ class PiPiRevenge(EventBasedAnimationClass):
         self.canvas.create_rectangle(x0,y0,x1,y1,
             fill = "midnight blue",width = 3)
         self.canvas.create_text(midX,midY,
-            font = "BritannicBold 24",text = "Start",fill = "white")
+            font = "Lato 24",text = "Start",fill = "white")
 
     def redrawAll(self):
         self.canvas.delete(ALL)
