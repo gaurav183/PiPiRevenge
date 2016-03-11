6c7dbc82de7a56ec38a7d622d3c5f3daaa4ee8f1
Namrita Murali <namrita.murali@gmail.com>
2016-03-11 10:01:26 -0500

    randomized asteroid falls

--- PiPiRevenge.py
+++ PiPiRevenge.py
@@ -16,8 +16,28 @@ class PiPiRevenge(EventBasedAnimationClass):
         self.startButtonX0,self.startButtonY0 = self.width/2-80,self.height/2.5
         self.startButtonX1 = self.width/2+80
         self.startButtonY1 = self.startButtonY0 + 70
-        self.startHeight = self.height/4
+        self.initPositions()
 
+
+    # all objects are treated as circles for collision detection
+    def initPositions(self):
+        self.redPlanetX = self.width/4
+        self.redPlanetY = 7*self.height/8
+        self.redPlanetR = 50
+        self.bluePlanetX = self.width/2
+        self.bluePlanetY = 7*self.height/8
+        self.bluePlanetR = 50
+        self.goldPlanetX = 3*self.width/4
+        self.goldPlanetY = 7*self.height/4
+        self.goldPlanetR = 50
+        self.asteroid1X = self.width/4
+        self.asteroid1Y = self.height/12
+        self.asteroid2X = self.width/2
+        self.asteroid2Y = self.height/12
+        self.asteroid3X = 3*self.width/4
+        self.asteroid3Y = self.height/12
+        self.asteroidR = 30
+        
     def drawSplashScreen(self):
         self.background = PhotoImage(file = "images/splashScreen.gif")
         self.canvas.create_image(self.width/2, self.height/2, image = self.background)
@@ -40,15 +60,21 @@ class PiPiRevenge(EventBasedAnimationClass):
         self.drawAsteroids()
     
     def onTimerFired(self):
-        self.startHeight += 15
-        if (self.startHeight >= (7*self.height/8)):
-          self.startHeight = self.height/4
+        self.asteroid1Y += random.randint(0,65)
+        self.asteroid2Y += random.randint(0,65)
+        self.asteroid3Y += random.randint(0,65)
+        if (self.asteroid1Y >= (7*self.height/8)):
+          self.asteroid1Y = self.height/12
+        if (self.asteroid2Y >= (7*self.height/8)):
+          self.asteroid2Y = self.height/12
+        if (self.asteroid3Y >= (7*self.height/8)):
+          self.asteroid3Y = self.height/12
 
     def drawAsteroids(self):
         self.asteroid = PhotoImage(file = "images/asteroid.gif")
-        self.canvas.create_image((self.width/4), self.startHeight, image = self.asteroid)
-        self.canvas.create_image((self.width/2), self.startHeight, image = self.asteroid)
-        self.canvas.create_image((3*self.width/4), self.startHeight, image = self.asteroid)
+        self.canvas.create_image(self.asteroid1X, self.asteroid1Y, image = self.asteroid)
+        self.canvas.create_image(self.asteroid2X, self.asteroid2Y, image = self.asteroid)
+        self.canvas.create_image(self.asteroid3X, self.asteroid3Y, image = self.asteroid)
 
     def drawStartButton(self):
         x0,y0 = self.startButtonX0,self.startButtonY0
